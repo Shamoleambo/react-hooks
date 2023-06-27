deda5fac35f1e93691f92f094df0fb9f8b25f4b0
Tiago Gomes <tidgomes@gmail.com>
2023-06-27 10:19:49 -0300

    refactor the Search component to use the custom hook useHttp

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect, useRef } from 'react'
 
 import Card from '../UI/Card'
+import ErrorModal from '../UI/ErrorModal'
 import './Search.css'
+import useHttp from '../../hooks/http'
 
 const Search = React.memo(({ onLoadIngredients }) => {
   const [filter, setFilter] = useState('')
   const filterInput = useRef()
+  const { isLoading, data, error, sendRequest, clear } = useHttp()
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -13,34 +16,41 @@ const Search = React.memo(({ onLoadIngredients }) => {
         const query =
           filter.length === 0 ? '' : `?orderBy="title"&equalTo="${filter}"`
 
-        fetch(
-          `https://react-hooks-eb96c-default-rtdb.firebaseio.com/ingredients.json${query}`
+        sendRequest(
+          `https://react-hooks-eb96c-default-rtdb.firebaseio.com/ingredients.json${query}`,
+          'GET'
         )
-          .then((response) => response.json())
-          .then((responseData) => {
-            const loadedIngredients = []
-            for (let key in responseData) {
-              loadedIngredients.push({
-                id: key,
-                title: responseData[key].title,
-                amount: responseData[key].amount
-              })
-            }
-            onLoadIngredients(loadedIngredients)
-          })
       }
     }, 500)
 
     return () => {
       clearTimeout(timer)
     }
-  }, [filter, onLoadIngredients])
+  }, [filter, filterInput, sendRequest])
+
+  useEffect(() => {
+    if (!isLoading && !error && data) {
+      const loadedIngredients = []
+
+      for (let key in data) {
+        loadedIngredients.push({
+          id: key,
+          title: data[key].title,
+          amount: data[key].amount
+        })
+      }
+
+      onLoadIngredients(loadedIngredients)
+    }
+  }, [data, isLoading, error, onLoadIngredients])
 
   return (
     <section className='search'>
+      {error && <ErrorModal onClose={clear}>{error}</ErrorModal>}
       <Card>
         <div className='search-input'>
           <label>Filter by Title</label>
+          {isLoading && <span>Loading...</span>}
           <input
             type='text'
             onChange={(event) => setFilter(event.target.value)}
